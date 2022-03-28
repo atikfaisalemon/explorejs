307cbe83e728ab94292dfc33646237830f649e6b
atikfaisalemon <atikfaisalemon@gmail.com>
2022-03-29 02:49:26 +0600

    condition

--- condition.js
+++ condition.js
@@ -1,5 +1,27 @@
+// when price is greater than 
 var price = 20;
 if(price>15)
+{
+    console.log("I will buy");
+}
+else{
+console.log("I will not buy");
+}
+
+// when price is Equal
+var price = 20;
+if(price == 15)
+{
+    console.log("I will buy");
+}
+else{
+console.log("I will not buy");
+}
+
+
+// when price is not Equal
+var price = 20;
+if(price != 20)
 {
     console.log("I will buy");
 }
