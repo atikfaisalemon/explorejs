2c75f257ea0686ea4cc465a682664e5512d3c324
atikfaisalemon <atikfaisalemon@gmail.com>
2022-04-04 01:34:46 +0600

    object

---
+++ object.js
@@ -0,0 +1,7 @@
+var student = {name:"emon", phone: 1234, id: 19203037};
+var student2 = {Name: "Tushar", phone:  321, id: 19203026};
+
+console.log(student);
+var phoneNo = student.phone;
+console.log(phoneNo);
+
