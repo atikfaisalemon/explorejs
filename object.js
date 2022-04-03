76d52015833025c3f888ba461884b06e210b2745
atikfaisalemon <atikfaisalemon@gmail.com>
2022-04-04 01:47:13 +0600

    number updatet

--- object.js
+++ object.js
@@ -2,6 +2,13 @@ var student = {name:"emon", phone: 1234, id: 19203037};
 var student2 = {Name: "Tushar", phone:  321, id: 19203026};
 
 console.log(student);
+
 var phoneNo = student.phone;
+var phoneNo2 = student2.phone;
+// phone number updated.
+student2.phone = 45642;
+
 console.log(phoneNo);
+console.log(student2);
+
 
