eb1a3dc5e7f7a41e53df46ffa1f730ffe6b418cd
atikfaisalemon <atikfaisalemon@gmail.com>
2022-04-14 00:34:54 +0600

    swapJavascript

--- swap.js
+++ swap.js
@@ -7,3 +7,13 @@ var temp= a;
 a =b;
 b= temp;
 console.log("after swap :", "a =", a ,"b =", b);
+
+console.log("*\n**\n***");
+// JavaScript
+
+var x= "Empon";
+var y= "Tushar";
+console.log("Before swap :", "x =", x ,"y =", y);
+
+[x, y] = [y, x];
+console.log("After swap :", "x =", x ,"y =", y);
